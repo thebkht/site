f1d98eec9647faae021a2faac1a62dc9e83dd30b
Bakhtiyor Ganijon <me@bkhtdev.com>
2024-08-13 18:26:02 +0500

    chore: Refactor Navbar component to remove unnecessary margin and add item alignment

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -14,13 +14,13 @@ const navItems = {
 
 export function Navbar() {
   return (
-    <aside className="-ml-[8px] mb-16 tracking-tight">
+    <aside className="mb-16 tracking-tight">
       <div className="lg:sticky lg:top-20">
         <nav
           className="flex flex-row items-start relative px-0 pb-0 fade md:overflow-auto scroll-pr-6 md:relative"
           id="nav"
         >
-          <div className="flex flex-row space-x-0 pr-10">
+          <div className="flex flex-row space-x-0 pr-10 items-center">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 70 70"
