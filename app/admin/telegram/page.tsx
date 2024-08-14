30d402c5ed4fc1ed60f6442d14e3c19c211197a6
Bakhtiyor Ganijon <me@bkhtdev.com>
2024-08-14 21:14:46 +0500

    chore: Add Telegram form component for managing guestbook entries

---
+++ app/admin/telegram/page.tsx
@@ -0,0 +1,27 @@
+import { auth } from 'app/auth';
+import { getGuestbookEntries } from 'app/db/queries';
+import { redirect } from 'next/navigation';
+import Form from './form';
+
+export const metadata = {
+  title: 'Telegram',
+};
+
+export default async function GuestbookPage() {
+  let session = await auth();
+  if (
+    session?.user?.email !== 'me@bkhtdev.com' &&
+    session?.user?.email !== 'b.yusupoff001@gmail.com'
+  ) {
+    redirect('/');
+  }
+
+  let notes = await getGuestbookEntries();
+
+  return (
+    <section>
+      <h1 className="font-medium text-2xl mb-8 tracking-tighter">telegram</h1>
+      <Form notes={notes} />
+    </section>
+  );
+}
