2745b71a6a3e29059265c59d95a82fb0350e8731
Bakhtiyor Ganijon <me@bkhtdev.com>
2024-08-15 00:20:56 +0500

    refactor: Update form input styling in app/admin/create-note/page.tsx

--- app/admin/create-note/page.tsx
+++ app/admin/create-note/page.tsx
@@ -25,14 +25,14 @@ export default function Form() {
         name="title"
         type="text"
         required
-        className="pl-4 pr-32 py-2 mt-1 focus:ring-blue-500 focus:border-blue-500 block w-full border-neutral-300 rounded-md bg-gray-100 dark:bg-neutral-800 text-neutral-900 dark:text-neutral-100"
+        className="px-4 py-2 mt-1 focus:ring-blue-500 focus:border-blue-500 block w-full border-neutral-300 rounded-md bg-gray-100 dark:bg-neutral-800 text-neutral-900 dark:text-neutral-100"
       />
       <textarea
         aria-label="Content"
         placeholder="Content..."
         name="content"
         required
-        className="pl-4 pr-32 py-2 mt-1 focus:ring-blue-500 focus:border-blue-500 block w-full border-neutral-300 rounded-md bg-gray-100 dark:bg-neutral-800 text-neutral-900 dark:text-neutral-100"
+        className="px-4 py-2 mt-1 focus:ring-blue-500 focus:border-blue-500 block w-full border-neutral-300 rounded-md bg-gray-100 dark:bg-neutral-800 text-neutral-900 dark:text-neutral-100"
       />
       <SubmitButton />
     </form>
