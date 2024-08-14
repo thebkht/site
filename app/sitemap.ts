c5de742a34f4c1be7a9ac0223118ded6e88ba375
Bakhtiyor Ganijon <me@bkhtdev.com>
2024-08-14 23:50:03 +0500

    feat: Add getNotes function to retrieve notes for sitemap generation

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,4 +1,5 @@
 import { getBlogPosts } from 'app/db/blog';
+import { getNotes } from './db/queries';
 
 export const baseUrl = 'https://blog.bkhtdev.com';
 
@@ -8,10 +9,17 @@ export default async function sitemap() {
     lastModified: post.metadata.publishedAt,
   }));
 
+  let notes = await getNotes().then((notes) =>
+    notes.map((note) => ({
+      url: `${baseUrl}/notes/${note.slug}`,
+      lastModified: note.publishedAt,
+    }))
+  );
+
   let routes = ['', '/guestbook', '/uses', '/notes'].map((route) => ({
     url: `${baseUrl}${route}`,
     lastModified: new Date().toISOString().split('T')[0],
   }));
 
-  return [...routes, ...blogs];
+  return [...routes, ...blogs, ...notes];
 }
