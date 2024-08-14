6d5cf67724297b7966262e767be8072f3584d9d0
Bakhtiyor Ganijon <me@bkhtdev.com>
2024-08-14 21:55:59 +0500

    refactor: Update NotePage component to handle empty notes list

--- app/notes/pages.tsx
+++ app/notes/pages.tsx
@@ -11,31 +11,37 @@ export default async function NotePage() {
       <h1 className="font-medium text-2xl mb-8 tracking-tighter">
         read my blog
       </h1>
-      {allNotes
-        .sort((a, b) => {
-          if (
-            new Date(a.metadata.publishedAt) > new Date(b.metadata.publishedAt)
-          ) {
-            return -1;
-          }
-          return 1;
-        })
-        .map((post) => (
-          <Link
-            key={post.slug}
-            className="flex flex-col space-y-1 mb-4"
-            href={`/notes/${post.slug}`}
-          >
-            <div className="w-full flex flex-col">
-              <p className="text-neutral-900 dark:text-neutral-100 tracking-tight">
-                {post.title}
-              </p>
-              <Suspense fallback={<p className="h-6" />}>
-                <Views slug={post.slug} />
-              </Suspense>
-            </div>
-          </Link>
-        ))}
+
+      {allNotes.length === 0 ? (
+        <p className="text-neutral-600 dark:text-neutral-400">No notes yet.</p>
+      ) : (
+        allNotes
+          .sort((a, b) => {
+            if (
+              new Date(a.metadata.publishedAt) >
+              new Date(b.metadata.publishedAt)
+            ) {
+              return -1;
+            }
+            return 1;
+          })
+          .map((post) => (
+            <Link
+              key={post.slug}
+              className="flex flex-col space-y-1 mb-4"
+              href={`/notes/${post.slug}`}
+            >
+              <div className="w-full flex flex-col">
+                <p className="text-neutral-900 dark:text-neutral-100 tracking-tight">
+                  {post.title}
+                </p>
+                <Suspense fallback={<p className="h-6" />}>
+                  <Views slug={post.slug} />
+                </Suspense>
+              </div>
+            </Link>
+          ))
+      )}
     </section>
   );
 }
