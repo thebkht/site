80f851dba71a351c92b7c2de54bcf83348f7cd0f
Bakhtiyor Ganijon <me@bkhtdev.com>
2024-08-15 09:57:52 +0500

    refactor: Add admin layout and navbar components

---
+++ app/admin/layout.tsx
@@ -0,0 +1,12 @@
+import { Navbar } from './nav';
+
+const cx = (...classes) => classes.filter(Boolean).join(' ');
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="flex-auto min-w-0 mt-6 flex flex-col">
+      <Navbar />
+      {children}
+    </div>
+  );
+}
