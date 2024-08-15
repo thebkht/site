60f82682c435315d6affa5434961c128811dbe8e
Bakhtiyor Ganijon <me@bkhtdev.com>
2024-08-15 10:00:37 +0500

    refactor: Update layout and navbar styling in admin pages

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -4,7 +4,7 @@ const cx = (...classes) => classes.filter(Boolean).join(' ');
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex-auto min-w-0 mt-6 flex flex-col">
+    <div className="flex-auto min-w-0 flex flex-col">
       <Navbar />
       {children}
     </div>
--- app/admin/nav.tsx
+++ app/admin/nav.tsx
@@ -14,24 +14,13 @@ const navItems = {
 
 export function Navbar() {
   return (
-    <aside className="mb-16 tracking-tight">
+    <aside className="mb-6 tracking-tight">
       <div className="lg:sticky lg:top-20">
         <nav
           className="flex flex-row items-start relative px-0 pb-0 fade md:overflow-auto scroll-pr-6 md:relative"
           id="nav"
         >
           <div className="flex flex-row space-x-0 pr-10 items-center">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 70 70"
-              className="h-5 w-5"
-            >
-              <path
-                d="M58.97,11.03v37.12l-10.71,10.82H11.03V21.85l10.71-10.82h37.22M70,0H17.15L0,17.31v52.69h52.85l17.15-17.31V0h0Z"
-                fill="currentColor"
-                strokeWidth="0"
-              />
-            </svg>
             {Object.entries(navItems).map(([path, { name }]) => {
               return (
                 <Link
