593b26079752f0880c029791c75581af80456c27
Bakhtiyor Ganijon <me@bkhtdev.com>
2024-08-20 21:43:42 +0900

    refactor: Add lazy loading for images in og/route.tsx

--- app/og/route.tsx
+++ app/og/route.tsx
@@ -92,20 +92,16 @@ export async function GET(req: Request) {
             tw="flex items-center text-xl"
             style={{ fontWeight: 'normal', letterSpacing: '-1px' }}
           >
-            <svg width="32" height="32" viewBox="0 0 48 48" fill="none">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 70 70"
+              height={48}
+              width={48}
+            >
               <path
-                d="M30 44v-8a9.6 9.6 0 0 0-2-7c6 0 12-4 12-11 .16-2.5-.54-4.96-2-7 .56-2.3.56-4.7 0-7 0 0-2 0-6 3-5.28-1-10.72-1-16 0-4-3-6-3-6-3-.6 2.3-.6 4.7 0 7a10.806 10.806 0 0 0-2 7c0 7 6 11 12 11a9.43 9.43 0 0 0-1.7 3.3c-.34 1.2-.44 2.46-.3 3.7v8"
-                stroke={paint}
-                stroke-width="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-              />
-              <path
-                d="M18 36c-9.02 4-10-4-14-4"
-                stroke={paint}
-                stroke-width="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
+                d="M58.97,11.03v37.12l-10.71,10.82H11.03V21.85l10.71-10.82h37.22M70,0H17.15L0,17.31v52.69h52.85l17.15-17.31V0h0Z"
+                fill="currentColor"
+                strokeWidth="0"
               />
             </svg>
             <div tw="flex ml-2">github.com/thebkht</div>
