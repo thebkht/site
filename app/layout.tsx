23341c45b9743118183288e08945e9995d256abf
Bakhtiyor Ganijon <b.yusupoff001@gmail.com>
2024-10-30 04:14:10 +0900

    Refactor layout.tsx: Remove unused main.css import

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,4 @@
 import './global.css';
-import './main.css';
 import type { Metadata } from 'next';
 import { Analytics } from '@vercel/analytics/react';
 import { SpeedInsights } from '@vercel/speed-insights/next';
