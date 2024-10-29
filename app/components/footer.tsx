0b1d8dc8471cee5c3fda18c6bdc9aa9b2aa07d08
Bakhtiyor Ganijon <b.yusupoff001@gmail.com>
2024-10-30 03:37:53 +0900

    Refactor footer component to use new color scheme

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -33,7 +33,7 @@ export default function Footer() {
           viewBox="0 0 70 70"
           fill="none"
           xmlns="http://www.w3.org/2000/svg"
-          className="h-5 w-5 text-muted-foreground"
+          className="h-5 w-5 text-muted"
         >
           <path
             fillRule="evenodd"
@@ -48,7 +48,7 @@ export default function Footer() {
               key={link.name}
               href={link.url}
               target="_blank"
-              className="text-muted-foreground hover:text-blue-500 transition-colors duration-200"
+              className="text-muted hover:text-blue-500 transition-colors duration-200"
             >
               {link.name}
             </a>
@@ -56,7 +56,7 @@ export default function Footer() {
             <Link
               key={link.name}
               href={link.url}
-              className="text-muted-foreground hover:text-blue-500 transition-colors duration-200"
+              className="text-muted hover:text-blue-500 transition-colors duration-200"
             >
               {link.name}
             </Link>
