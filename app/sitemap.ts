1315a136d950986d010dd19511b832f1c557c0a9
Bakhtiyor Ganijon <b.yusupoff001@gmail.com>
2024-11-07 00:23:10 +0900

    Refactor content structure: rename and update kebab-case and rebranding pages with metadata and animated components

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,12 +1,33 @@
 import { getBlogPosts } from 'app/db/blog';
-import { getNotes } from './db/queries';
+import { promises as fs } from 'fs';
+import path from 'path';
 
 export const baseUrl = 'https://bkhtdev.com';
 
+async function getNoteSlugs(dir: string) {
+  const entries = await fs.readdir(dir, {
+    recursive: true,
+    withFileTypes: true,
+  });
+  return entries
+    .filter((entry) => entry.isFile() && entry.name === 'page.mdx')
+    .map((entry) => {
+      const relativePath = path.relative(
+        dir,
+        path.join(entry.parentPath, entry.name)
+      );
+      return path.dirname(relativePath);
+    })
+    .map((slug) => slug.replace(/\\/g, '/'));
+}
+
 export default async function sitemap() {
-  let blogs = getBlogPosts().map((post) => ({
-    url: `${baseUrl}/p/${post.slug}`,
-    lastModified: post.metadata.publishedAt,
+  const notesDirectory = path.join(process.cwd(), 'app', 'p');
+  const slugs = await getNoteSlugs(notesDirectory);
+
+  const notes = slugs.map((slug) => ({
+    url: `${baseUrl}/p/${slug}`,
+    lastModified: new Date().toISOString(),
   }));
 
   let routes = ['', '/guestbook', '/blog', '/work', '/stack'].map((route) => ({
@@ -14,5 +35,5 @@ export default async function sitemap() {
     lastModified: new Date().toISOString().split('T')[0],
   }));
 
-  return [...routes, ...blogs];
+  return [...routes, ...notes];
 }
