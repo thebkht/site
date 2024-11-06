b513ed834e46a97f1e9857e6f682c3d2239f5bb0
Bakhtiyor Ganijon <b.yusupoff001@gmail.com>
2024-11-07 00:32:24 +0900

    Refactor sitemap: replace note slugs with blog posts and simplify directory handling

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,33 +1,12 @@
 import { getBlogPosts } from 'app/db/blog';
-import { promises as fs } from 'fs';
-import path from 'path';
+import { getNotes } from './db/queries';
 
 export const baseUrl = 'https://bkhtdev.com';
 
-async function getNoteSlugs(dir: string) {
-  const entries = await fs.readdir(dir, {
-    recursive: true,
-    withFileTypes: true,
-  });
-  return entries
-    .filter((entry) => entry.isFile() && entry.name === 'page.mdx')
-    .map((entry) => {
-      const relativePath = path.relative(
-        dir,
-        path.join(entry.parentPath, entry.name)
-      );
-      return path.dirname(relativePath);
-    })
-    .map((slug) => slug.replace(/\\/g, '/'));
-}
-
 export default async function sitemap() {
-  const notesDirectory = path.join(process.cwd(), 'app', 'p');
-  const slugs = await getNoteSlugs(notesDirectory);
-
-  const notes = slugs.map((slug) => ({
-    url: `${baseUrl}/p/${slug}`,
-    lastModified: new Date().toISOString(),
+  let blogs = getBlogPosts().map((post) => ({
+    url: `${baseUrl}/p/${post.slug}`,
+    lastModified: post.metadata.publishedAt,
   }));
 
   let routes = ['', '/guestbook', '/blog', '/work', '/stack'].map((route) => ({
@@ -35,5 +14,5 @@ export default async function sitemap() {
     lastModified: new Date().toISOString().split('T')[0],
   }));
 
-  return [...routes, ...notes];
+  return [...routes, ...blogs];
 }
