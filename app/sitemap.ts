c926bd285484e61a1731795c43f0090d38d6eeb6
Bakhtiyor Ganijon <b.yusupoff001@gmail.com>
2024-11-07 00:39:47 +0900

    Refactor sitemap: enhance note handling by retrieving slugs from directories and include notes in the sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,18 +1,49 @@
+import { promises as fs } from 'fs';
+import path from 'path';
 import { getBlogPosts } from 'app/db/blog';
 import { getNotes } from './db/queries';
 
 export const baseUrl = 'https://bkhtdev.com';
 
+async function getNoteSlugs(dir: string) {
+  const entries = await fs.readdir(dir, {
+    withFileTypes: true,
+  });
+  const slugs = await Promise.all(
+    entries.map(async (entry) => {
+      if (entry.isDirectory()) {
+        const subDir = path.join(dir, entry.name);
+        const subEntries = await fs.readdir(subDir, {
+          withFileTypes: true,
+        });
+        const pageFile = subEntries.find(
+          (subEntry) => subEntry.isFile() && subEntry.name === 'page.mdx'
+        );
+        if (pageFile) {
+          return entry.name;
+        }
+      }
+      return null;
+    })
+  );
+  return slugs.filter(Boolean);
+}
+
 export default async function sitemap() {
-  let blogs = getBlogPosts().map((post) => ({
-    url: `${baseUrl}/p/${post.slug}`,
-    lastModified: post.metadata.publishedAt,
+  const notesDirectory = path.join(process.cwd(), 'app', 'p');
+  const slugs = await getNoteSlugs(notesDirectory);
+
+  const notes = slugs.map((slug) => ({
+    url: `${baseUrl}/p/${slug}`,
+    lastModified: new Date().toISOString(),
   }));
 
-  let routes = ['', '/guestbook', '/blog', '/work', '/stack'].map((route) => ({
-    url: `${baseUrl}${route}`,
-    lastModified: new Date().toISOString().split('T')[0],
-  }));
+  const routes = ['', '/guestbook', '/blog', '/work', '/stack'].map(
+    (route) => ({
+      url: `${baseUrl}${route}`,
+      lastModified: new Date().toISOString().split('T')[0],
+    })
+  );
 
-  return [...routes, ...blogs];
+  return [...routes, ...notes];
 }
